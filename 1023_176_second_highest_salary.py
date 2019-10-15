90b425f4b8ad0724ec157cdae071db8b2f8bd803
pre20myungwoo <myungwoo.ko@gmail.com>
2019-10-16 08:46:21 +0900

    till 1024 factorial

---
+++ 1023_176_second_highest_salary.py
@@ -0,0 +1,111 @@
+"""
+176. Second Highest Salary
+Easy
+
+582
+
+311
+
+Favorite
+
+Share
+SQL Schema
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+
+select * from employee order by salary desc limit 1,1
+Description : limit x,y
+x: The row offset from which you want to start displaying records. For nth record it will be n-1.
+y: The number of records you want to display. (Always 1 in this case)
+
+
+select salary from table order by salary desc limit 0,1
+// for highest salary of table
+select salary from table order by salary desc limit 1,1
+// for second highest salary
+
+*******
+query *
+*******
+
+- LIMIT A B
+A = {시작지점index느낌(0부터유효)}
+B = {시작지점부터개수(1부터count)}
+
+- IFNULL
+SELECT TaskCode
+From Tasks;
+Result:
+
++----------+
+| TaskCode |
++----------+
+| gar123   |
+| NULL     |
+| NULL     |
+| dog456   |
+| NULL     |
+| cat789   |
++----------+
+So we have three NULL values and three non-NULL values.
+
+The IFNULL() Function
+Given its name, this is probably the most obvious option for replacing NULL values in MySQL. This function is basically the equivalent of ISNULL() in SQL Server.
+
+The IFNULL() function allows you to provide two arguments. The first argument is returned only if it is not NULL. If it is NULL, then the second argument is returned instead.
+
+Here’s an example of using IFNULL() against our sample data set:
+
+SELECT IFNULL(TaskCode, 'N/A') AS Result
+FROM Tasks;
+Result:
+
++--------+
+| Result |
++--------+
+| gar123 |
+| N/A    |
+| N/A    |
+| dog456 |
+| N/A    |
+| cat789 |
++--------+
+"""
+
+
+
+"""
+아래의 input에 대해서 
+#1 query는 [] => value가 아예 없음으로 리턴하고,
+#2 query는 [null] => value null(값이 없음)으로 결과를 리턴한다. 
+ 
+input {"headers": {"Employee": ["Id", "Salary"]}, "rows": {"Employee": [[1, 100]]}}
+"""
+#1
+"""
+select Salary as SecondHighestSalary from Employee order by Salary desc limit 1,1
+"""
+#=> {"headers": ["SecondHighestSalary"], "values": [[]]}
+
+#2
+"""
+Select MAX(Salary) as SecondHighestSalary from Employee where Salary < (Select MAX(Salary) from Employee)
+"""
+#=> {"headers": ["SecondHighestSalary"], "values": [[null]]}
+
+
---
+++ 1024_**factorial_trailing_zeros.py
@@ -0,0 +1,56 @@
+"""
+172. Factorial Trailing Zeroes
+Easy
+
+Share
+Given an integer n, return the number of trailing zeroes in n!.
+
+Example 1:
+
+Input: 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+
+We add a trailing zero every time we multiply by 10 (5 * 2). Since we will have always more 2s than 5s, ***5만큼 커지면 2만큼은 두번 커지니까
+the problem is to find the number of 5s in the numbers from 1 to n.
+Let's consider 10! as example:
+
+10! = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+In this case, we have two 5s (in 10 and 5), so the result is 2. How can we efficiently compute this number for a given n?
+Well, we could compute the multiple of 5 contained in n, but this is not enough: for example, 25 accounts for 2 5s, 50 accounts also for 2 5s, and so on.
+5가 여러개 있는 경우 그 여러번을 모두 카운트해준다. 5의 배수중 중복가능성을 고려해 체크해주는 수는 최대 n(인자로 들어온) 자신을 최대로 한다.
+* 두번째 돌면서는 두번 들어간거 체크, 세번째 돌면서는 세번 들어간거 체크...
+
+So, we do the following: we start from 5, and we see how many multiples of 5 we have in n. Then, we multiply 5 by 5 (25)
+and we add how many multiples of 25 we have in n. In this case we will not have duplicates, as at in each step we will add only one 5 to the result.
+"""
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        d, count = 5, 0
+        while d <= n:
+            count += n // d
+            d *= 5
+        return count
+
+    def trailingZeroes2(self, n):
+        k, tot = 5, 0
+        while k <= n:
+            tot += n // k
+            k = k * 5
+        return tot
+
+
+s = Solution()
+test = s.trailingZeroes(50)
+
+print(1, test)
