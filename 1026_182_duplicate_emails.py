1a209876087a4c922ace5a49159e56a02bb799fe
pre20myungwoo <myungwoo.ko@gmail.com>
2019-10-17 22:51:02 +0900

    till 1027

---
+++ 1026_182_duplicate_emails.py
@@ -0,0 +1,31 @@
+"""
+182. Duplicate Emails
+Easy
+
+Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+"""
+
+"""
+SELECT 
+    email
+FROM
+    person
+GROUP BY email
+HAVING COUNT(email) > 1;
+
+"""
