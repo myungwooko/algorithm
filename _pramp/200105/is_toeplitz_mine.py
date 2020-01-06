c86abd6fb42aaabd889551aa7f6c895d4c5e9871
pre20myungwoo <myungwoo.ko@gmail.com>
2020-01-05 17:48:14 -0800

    pramps 200105 - is toeplitz - mine

--- _pramp/200105/is_toeplitz_mine.py
+++ _pramp/200105/is_toeplitz_mine.py
@@ -20,6 +20,13 @@ def isToeplitz(arr):
     return True
 
 
+def isToeplitz(arr):
+        for i in range(len(arr) - 1):
+            for j in range(len(arr[0]) - 1):
+                if arr[i][j] != arr[i + 1][j + 1]:
+                    return False
+        return True
+
 arr = [[1, 2, 3, 4],
        [5, 1, 2, 3],
        [6, 3, 1, 2]]
