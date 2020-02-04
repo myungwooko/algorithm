7a6311d77d9ebf7825e507b0980d67067689c8e3
pre20myungwoo <myungwoo.ko@gmail.com>
2020-02-04 09:09:43 -0800

    interviewing.io -
    Interview
    Lexpect
     Lgeneral
     - kth smallest element in a bst

--- _interviewing.io/200203_availableTime.py
+++ _interviewing.io/200203_availableTime.py
@@ -16,12 +16,6 @@
 ]
 
 => [(3, 5), (7, 8)]
-
-
-** if it's hard to explain, it is hard to code <= 맞는 말이다.
-** don't fixate to that complex one
-** find out another approach
-** 간단하게 고치면 되는 것들을 오늘 왜 그랬을까, 영어도 그렇고 => 더 많이 연습할 것, pramp + a
 '''
 
 
@@ -35,8 +29,8 @@ def availableTime(times):
     idx = 0
     res = []
     while idx < len(times) - 1:
-        if times[idx][1] > times[idx + 1][0] or times[idx][1] > times[idx + 1][1]:
-            times[idx] = (min(times[idx][0], times[idx + 1][0]), max(times[idx][1], times[idx + 1][1]))
+        if times[idx][1] > times[idx + 1][0]:
+            times[idx] = times[idx][0], times[idx][1]
             times.pop(idx + 1)
             continue
         else:
