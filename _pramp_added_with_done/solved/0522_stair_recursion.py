38f07e9b0d7d77dafb6f3d0e9441340502db3122
pre20myungwoo <myungwoo.ko@gmail.com>
2020-05-23 10:35:39 +0900

    0523_stair_recursion_compare

--- _pramp_added_with_done/solved/0522_stair_recursion.py
+++ _pramp_added_with_done/solved/0522_stair_recursion.py
@@ -6,3 +6,20 @@ def c(x):
 
 test = c(10)
 print(test)
+
+
+#mine was
+def c(x):
+    count = [0]
+    def helper(n):
+        if n == x:
+            count[0] += 1
+        elif n < x:
+            helper(n+1)
+            helper(n+2)
+    helper(0)
+    return count[0]
+
+
+test = c(10)
+print(test)
