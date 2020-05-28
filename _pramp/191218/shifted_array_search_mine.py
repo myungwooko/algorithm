a8bbbeebfa222376f8e07cd996c95fedb5ab4faa
pre20myungwoo <myungwoo.ko@gmail.com>
2020-05-28 09:47:49 +0900

    update 0527

--- _pramp/191218/shifted_array_search_mine.py
+++ _pramp/191218/shifted_array_search_mine.py
@@ -25,72 +25,30 @@ Constraints:
 
 try it: https://leetcode.com/problems/search-in-rotated-sorted-array/
 """
-def get_pivot(shiftArr):
-    pivot = 0
-    for i, v in enumerate(shiftArr):
-        if i + 1 < len(shiftArr) and shiftArr[i] > shiftArr[i + 1]:
-            pivot = i
-            break
-    return pivot
-
-
+# Time Complexity: O(logn)
+# Space Complexity: O(1)
 def shifted_arr_search(shiftArr, num):
     l, r = 0, len(shiftArr) - 1
     while l <= r:
         mid = (l + r) // 2
         if shiftArr[mid] == num:
             return mid
-        elif shiftArr[l] < shiftArr[mid]:
+        elif shiftArr[mid] < shiftArr[r]:
+            if shiftArr[mid] < num <= shiftArr[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        else:
             if shiftArr[l] <= num < shiftArr[mid]:
                 r = mid - 1
             else:
                 l = mid + 1
-        else:
-            l = mid + 1
-    return -1
-
-
-# more simple
-# Time: O(logn)
-# Space: O(1)
-def shifted_arr_search(shiftArr, num):
-    l, r = 0, len(shiftArr) - 1
-    while l <= r:
-        mid = (l + r) // 2
-        if shiftArr[mid] == num:
-            return mid
-
-        if shiftArr[l] < shiftArr[mid]:
-            if shiftArr[l] <= num < shiftArr[mid]:
-                r = mid - 1
-                continue
-        l = mid + 1
 
     return -1
 
 
-shiftArr = [5, 6, 7, 8, 1, 2, 3]
+shiftArr = [9, 12, 17, 2, 4, 5]
 num = 2
+
 test = shifted_arr_search(shiftArr, num)
-print(test==5)
-
-shiftArr = [0]
-num = 0
-test2 = shifted_arr_search(shiftArr, num)
-print(test2==0)
-
-"""
-1. shiftArr = [9, 12, 17, 2, 4, 5], num = 2
-               ^                ^         
-                                       
-2. shiftArr = [2, 4, 5 ,9 ,12 ,17], num = 2
-
-
-l, h = 0, len(stiftArr)-1
-m = (l+h)//2
--
-get_pivot
-arr = [7,8,9,1,2,3,4,5]
-           ^
-=> return 2
-"""
+print(test)
