91f4ea5431a7f0842cc78f23e953e9ebe9aca6b6
pre20myungwoo <myungwoo.ko@gmail.com>
2020-06-07 12:44:36 +0900

    update 0607

--- _pramp/200305/decrypt_message_mine.py
+++ _pramp/200305/decrypt_message_mine.py
@@ -1,66 +1,19 @@
 """
-Decrypt Message
-An infamous gang of cyber criminals named “The Gray Cyber Mob”, which is behind many hacking attacks and drug trafficking,
-has recently become a target for the FBI. After intercepting some of their messages, which looked like complete nonsense,
-the agency learned that they indeed encrypt their messages, and studied their method of encryption.
-
-Their messages consist of lowercase latin letters only, and every word is encrypted separately as follows:
-
-Convert every letter to its ASCII value. Add 1 to the first letter, and then for every letter from the second one to the last one,
-add the value of the previous letter. Subtract 26 from every letter until it is in the range of lowercase letters a-z in ASCII.
-Convert the values back to letters.
-
-For instance, to encrypt the word “crime”
-
-Decrypted message:	c	r	i	m	e
-Step 1:	99	114	105	109	101
-Step 2:	100	214	319	428	529
-Step 3:	100	110	111	116	113
-Encrypted message:	d	n	o	t	q
-The FBI needs an efficient method to decrypt messages. Write a function named decrypt(word) that receives a string that
-consists of small latin letters only, and returns the decrypted word.
-
-Explain your solution and analyze its time and space complexities.
-
-Examples:
-
-input:  word = "dnotq"
-output: "crime"
-
-input:  word = "flgxswdliefy"
-output: "encyclopedia"
-Since the function should be used on messages with many words, make sure the function is as efficient as possible in both time and space.
-Explain the correctness of your function, and analyze its asymptotic runtime and space complexity.
-
-Note: Most programing languages have built-in methods of converting letters to ASCII values and vica versa.
-You may search the internet for the appropriate method.
-
-Constraints:
-
-[time limit] 5000ms
-
-[input] string word
-
-The ASCII value of every char is in the range of lowercase letters a-z.
-[output] string
-"""
-
-"""
 ##Encryption
 
-input => crime 
+input => crime
 
-1. 
+1.
 ord(c) => 99
 ord(r) => 114
 ord(i) => 105
 ord(m) => 109
 ord(e) => 101
 
-2. 
+2.
 99 + 1 => 100
 
-3. 
+3.
 100
 114 => 214
 105 => 319
@@ -83,6 +36,7 @@ char = chr(number)
 
 
 ------------------------------------------------------------------------------
+
 ## Dcryption
 
 step1: origin
@@ -97,28 +51,35 @@ step3: enc
 # As next lev2[i-1]
 => lev2[i] = lev2[i-1] + origin[i]
 """
-# lev3[n] = lev2[n-1] + lev1[n] - 26*n
-# lev1[n] = lev3[n] - lev2[n-1] + 26*n *96 < lev1[n] < 123
 
-# Time O(n)  <= O(n+k) N is for one for loop, k is process of finding m, O(n+k) => O(n)
-# Space complexity: O(3n)
+
+# the range of lowercase letters a-z in ASCII: 97-122
+# Formula for second~last: word[n] = lev1[n] + lev2[n-1] - 26*m
+# lev1[n] = word[n] - lev2[n-1] + 26*m
+# lev2[n] = lev2[n-1] + lev1[n]
+
+# Time O(n)  <= O(n+k) N is for one for loop, k is process of finding m
+# Space complexity: O(n) <= O(3n)
 def decrypt(word):
     if not word:
         return word
-    lev3 = [ord(c) for c in word]
+
+    lev3 = [ord(char) for char in word]
     lev2 = [lev3[0]]
     lev1 = [lev3[0] - 1]
 
-    for i in range(1, len(lev3)):
-        seg = lev3[i] - lev2[i - 1]
-        print(seg)
-        count = 1
-        n = 1
-        while seg + 26 * n < 97:
-            n += 1
-        lev1.append(seg + 26 * n)
+    for n in range(1, len(lev3)):
+        before_add_26m = lev3[n] - lev2[n - 1]
+        m = 1
+        while before_add_26m + 26 * m < 97:
+            m += 1
+        lev1_nth = before_add_26m + 26 * m
+        lev1.append(lev1_nth)
+        lev2_nth = lev2[n - 1] + lev1[n]
+        lev2.append(lev2_nth)
 
-        add_lev2 = lev2[i - 1] + lev1[i]
-        lev2.append(add_lev2)
+    return "".join([chr(asc) for asc in lev1])
 
-    return "".join([chr(n) for n in lev1])
+word = "dnotq"
+test = decrypt(word)
+print(test == "crime")
