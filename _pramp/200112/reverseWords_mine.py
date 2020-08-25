c7fe9ad9b98dbc6dcfc2b993762a550b0d14e8b1
pre20myungwoo <myungwoo.ko@gmail.com>
2020-08-25 09:18:42 +0900

    reverse word update

--- _pramp/200112/reverseWords_mine.py
+++ _pramp/200112/reverseWords_mine.py
@@ -1,31 +1,4 @@
 """
-Sentence Reverse
-You are given an array of characters arr that consists of sequences of characters separated by space characters. Each space-delimited sequence of characters defines a word.
-
-Implement a function reverseWords that reverses the order of the words in the array in the most efficient manner.
-
-Explain your solution and analyze its time and space complexities.
-
-Example:
-
-input:  arr = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', '  ',
-                'm', 'a', 'k', 'e', 's', '  ',
-                'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ]
-
-output: [ 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e', '  ',
-          'm', 'a', 'k', 'e', 's', '  ',
-          'p', 'e', 'r', 'f', 'e', 'c', 't' ]
-Constraints:
-
-[time limit] 5000ms
-
-[input] array.character arr
-
-0 ≤ arr.length ≤ 100
-[output] array.character
-
-"""
-"""
 Brute Force: O(N) Time with O(N) Space
 
 Space Saving Solution: O(N) Time with O(1) Space
@@ -34,24 +7,24 @@ arr = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', '  ',
         'm', 'a', 'k', 'e', 's', '  ',
          p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ]
          r
-         
+
          perfect makes practice
          l
                 r
          ecitcarp sekam tcefrep
          practice makes perfect
- 
-1. arr[::-1]      
+
+1. arr[::-1]
      "" => space = arr.pop(7) => arr.insert(0)
-           char => arr.pop(theIndex) 
-                => insIdx = 0 from to acc till meet space  
-         
+           char => arr.pop(theIndex)
+                => insIdx = 0 from to acc till meet space
+
       [ 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e', '  ',
         'm', 'a', 'k', 'e', 's', '  ',
         'p', 'e', 'r', 'f', 'e', 'c', 't' ]
-        
+
         => ["practice"]
-        
+
 Multiple Traversals
 Multiple Pointers
 variables l and r
@@ -85,26 +58,26 @@ def reverse_words(arr):
 
 
 # Time Complexity: O(n)
-# Space Complexity: O(1)
+# Space complexity: O(1)
 def reverse_words(arr):
   arr = arr[::-1]
-  last_idx = len(arr)-1
+  last_idx = len(arr) - 1
 
   for i in range(len(arr)):
-    if arr[i] == " ":
+    if arr[i] == ' ':
       continue
 
-    if i == 0 or (i > 0 and arr[i-1] == " "):
+    if i == 0 or (i > 0 and arr[i-1] == ' '):
       left = i
-      continue
 
-    right = i
-    if (right < last_idx and arr[right+1] == " ") or right == last_idx:
+    # the condition of 'left' part dosen't need to be added because left must have been set before 'right'
+    # but wanted to show it explicitly so that it can help understanding easily.
+    if (left == 0 or left) and (i < last_idx and arr[i+1] == ' ') or i == last_idx:
+      right = i
       while left < right:
         arr[left], arr[right] = arr[right], arr[left]
         left += 1
         right -= 1
-
   return arr
 
 
